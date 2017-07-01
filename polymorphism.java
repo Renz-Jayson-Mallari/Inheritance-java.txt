8a58dafc79767a374a4b8fa3e61bd39834a39ee3
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-06-30 21:32:15 -0400

    polymorphism examples added

--- polymorphism.java
+++ polymorphism.java
@@ -11,6 +11,8 @@ public class Deer extends Animal implements Vegetarian{}
 
 // Example 1
 
+
+// Support class file ...
 public class Employee{
 	private String name;
 	private String address;
@@ -41,5 +43,53 @@ public class Employee{
 	
 	public int getNumber(){
 		return number;
+	}
+}
+
+// Util class file ...
+/* Filename: Salary.java */
+public class Salary extends Employee{
+	
+	private double salary; // Yearly
+	
+	public Salary(String name , String address, int number, double salary){
+		super(name, address, number);
+		setSalary(salary);
+	}
+	
+	public void mailCheck(){
+		System.out.println("Within mailCheck of Salary class");
+		System.out.println("Mailing check to " + getName() + "with the salary of " + salary);
+	}
+	
+	public double getSalary(){
+		return salary;
+	}
+	
+	public double setSalary(double newSalary){
+		if (newSalary >= 0.0){
+			salary = newSalary;
+		}
+	}
+	
+	public double computeSalary(){
+		System.out.println("Computing the salary for " + getName() );
+		return salary/52;
+	}
+}
+
+//Main file
+public class VirtualDemo{
+	
+	public static void main(String[] args){
+		Salary s = new Salary("Isabelle-Lu", "Bayside,NY" , 3 , 45_000);
+		Employee e = new Salary("Genel Mallari", "Emlhurst, NY", 5, 41_000);
+		
+		System.out.prinln("Invoking mailCheck using Salary reference");
+		s.mailCheck();
+		
+		System.out.prinln("Invoking mailCheck using Employee reference");
+		e.mailCheck();
+		
 	}
 }
