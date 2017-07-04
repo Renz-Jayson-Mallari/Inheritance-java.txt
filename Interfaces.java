ae673a95ea9d63125f320fa6dc168b2d8c49db0d
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-07-03 23:29:35 -0400

    added interface example.

--- Interfaces.java
+++ Interfaces.java
@@ -1,1 +1,35 @@
 // Interfaces
+
+// Declaring interfaces
+import java.lang.*;
+
+public interface NameOfTheInterface{
+}
+
+//Example
+//Support files
+interface Animal{
+    
+    public void eat();
+    public void travel();
+}
+//MAin files 
+public class MammalDemo implements Animal{
+    
+    public void eat(){
+        System.out.println("Mammal can eat .");
+    }
+    
+    public void travel(){
+        System.out.println("Mammal can travel.");
+    }
+    
+    public static void main(String[] args){
+        MammalDemo mam = new MammalDemo();
+        
+        mam.eat();
+        mam.travel();
+        
+    }
+}
+	
