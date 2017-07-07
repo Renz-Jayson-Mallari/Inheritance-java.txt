969736341155adb6bcc7e218d4a0743c111499e3
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-07-06 22:22:05 -0400

    added package example

--- packages.java
+++ packages.java
@@ -1,1 +1,12 @@
 //packages
+
+// javac -d Destination_folder file_name.java
+
+//Example
+/* File name : Animal.java */
+package animals;
+
+interface Animal {
+   public void eat();
+   public void travel();
+}
