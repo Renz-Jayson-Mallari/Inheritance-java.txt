47f6ef507c538e9188ef43ad5df4a44897d18a01
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-07-07 20:35:17 -0400

    added package 2nd example

--- packages.java
+++ packages.java
@@ -10,3 +10,36 @@ interface Animal {
    public void eat();
    public void travel();
 }
+
+//another example
+// Support class
+package animals;
+
+interface Animals{
+    
+    public void eat();
+    public void travel();
+}
+
+//Main Class
+
+
+package animals;
+
+public class MammalInt{
+    
+    public void eat(){
+        System.out.println("Mammals eats...");
+    }
+    
+    public void travel(){
+        System.out.println("Mammal travels ... " );
+    }
+    
+    public static void main(String[] args){
+        MammalInt mal = new MammalInt();
+        
+        mal.eat();
+        mal.travel();
+    }
+}
