dbdaeb378eb00176eb0a6bede22e3593482fd669
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-07-09 14:09:45 -0400

    initialize data structures topic

---
+++ DataStructures.java
@@ -0,0 +1,15 @@
+// Data Structures
+
+/*The Enumeration
+		-- defines the method by which you can enumerate
+	## Method and description ##
+		1-- boolean hasElements()
+			-- must return true when there is more elements to extract, and false when all the elements has been enumerated.
+		2-- Object nextElement()
+			-- This returns the next object in the enumeration as a generic Object reference.
+			
+	## Example ##
+*/	
+
+
+ 
