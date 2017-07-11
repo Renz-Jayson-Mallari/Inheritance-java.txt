3fb2b8b14619e1e5037dca89f4fa51bc514fdfab
Renz-Jayson-Mallari <mallarirjc@yahoo.com>
2017-07-11 08:03:56 -0400

    added data Structure Enumeration example

--- DataStructures.java
+++ DataStructures.java
@@ -1,15 +1,42 @@
 // Data Structures
 
 /*The Enumeration
-		-- defines the method by which you can enumerate
+		-- defines method by which you can enumerate
 	## Method and description ##
 		1-- boolean hasElements()
-			-- must return true when there is more elements to extract, and false when all the elements has been enumerated.
+			-- it must return true when there is more elements to enumerate and false when all the elements has been enumerated.
 		2-- Object nextElement()
-			-- This returns the next object in the enumeration as a generic Object reference.
+			-- this will return the next Object on the enumeration as the Object reference.
 			
 	## Example ##
 */	
+
+import java.util.Vector;
+import java.util.Enumeration;
+
+public class Enumeration{
+	
+	public static void main(String[] args){
+		
+		Enumeration names;
+		Vector pNames = new Vector();
+		
+		pNames.add("Max");
+		pNames.add("Ian");
+		pNames.add("Renz");
+		pNames.add("Jayson");
+		pNames.add("Izzy");
+		
+		names = pNames.elements();
+		
+		while(names.hasMoreElements()){
+			System.out.println(names.nextElelemnts());
+		}
+		
+	}
+	
+}
+
 
 
  
